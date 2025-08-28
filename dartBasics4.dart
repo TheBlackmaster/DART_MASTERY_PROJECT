43345eda37f19a1216caaaa079cd8528efa69176
Moses <bigdang42@gmail.com>
2025-08-28 14:23:46 +0100

    String interpolation basics with inventory details

---
+++ dartBasics4.dart
@@ -0,0 +1,16 @@
+// Variable Declaration, Initialization, Printing, String Interpolation
+
+void main() {
+  String drugName;
+  String lotNumber;
+  int expiryYear;
+
+  drugName = "Ibuprofen 400mg";
+  lotNumber = "IBU-2025A";
+  expiryYear = 2027;
+
+  print("Drug: $drugName");
+  print("Lot Number: $lotNumber");
+  print("Expiry Year: $expiryYear");
+
+}
