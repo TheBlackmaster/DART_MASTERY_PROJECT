f2cedbe320aea01abeaa44b2704e1cbd5cbc333f
Moses <bigdang42@gmail.com>
2025-09-04 22:34:29 +0100

    new file

--- functions2.dart
+++ functions2.dart
@@ -1,20 +1,28 @@
 // Named arguments, Default values, Fat arrow, Scope
 // Create a function to calculate the total daily dose of a medication for a patient, with flexibility in arguments.
-void main(){
-  final dose = calculateDose(dosePerTablet: 500, frequencyPerDay: 2, drugName: 'Metformin');
-print(dose);
+void main() {
+  final dose = calculateDose(
+    dosePerTablet: 500,
+    frequencyPerDay: 2,
+    drugName: 'Metformin',
+  );
+  print(dose);
 }
 
-calculateDose({required String drugName, required int dosePerTablet, required int frequencyPerDay,int durationDays = 7}){
-int dailyDose = dosePerTablet * frequencyPerDay;
-int weeklyDose = dailyDose * durationDays;
+calculateDose({
+  required String drugName,
+  required int dosePerTablet,
+  required int frequencyPerDay,
+  int durationDays = 7,
+}) {
+  int dailyDose = dosePerTablet * frequencyPerDay;
+  int weeklyDose = dailyDose * durationDays;
 
-mgToGrams(int mg) => mg/1000;
+  mgToGrams(int mg) => mg / 1000;
 
-return '''
+  return '''
 Drug Name: $drugName
 Daily Dose: $dailyDose mg (${mgToGrams(dailyDose)} g)
 Total over 7 days: $weeklyDose mg (${mgToGrams(weeklyDose)} g)
 ''';
 }
-
